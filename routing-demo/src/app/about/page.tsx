88d4504d9cc8448a4aa98edcbaeca8d62b9c723c
quyhp <quyhp@runsystem.net>
2025-07-21 15:06:57 +0700

    5. routing

---
+++ routing-demo/src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <h1>Welcome to the about page</h1>;
+};
+
+export default AboutPage;
---
+++ routing-demo/src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+const ProfilePage = () => {
+  return <h1>Welcome to the profile page</h1>;
+};
+
+export default ProfilePage;
