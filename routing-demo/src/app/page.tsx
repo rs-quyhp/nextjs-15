2ad7b57c97f1e6b363f1d60982bdd2d592e2068f
quyhp <quyhp@runsystem.net>
2025-07-21 17:07:27 +0700

    19. link component

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <h1>Welcome to the home page</h1>;
+  return (
+    <>
+      <h1>Welcome to the home page</h1>
+      <p>
+        <Link href={'/blog'}>Blog</Link>
+      </p>
+      <Link href={'/products'}>Products</Link>
+    </>
+  );
 };
 
 export default HomePage;
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+
 const ProductsPage = () => {
-  return <h1>Welcome to the products page</h1>;
+  return (
+    <>
+      <h1>Welcome to the products page</h1>
+      <p>
+        <Link href={'/products/1'}>Product 1</Link>
+      </p>
+      <p>
+        <Link href={'/products/2'} replace>
+          Product 2
+        </Link>
+      </p>
+      <p></p>
+    </>
+  );
 };
 
 export default ProductsPage;
