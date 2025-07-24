eb26f6b1433024d12ed8aa4e7d000f0f666ddfa5
quyhp <quyhp@runsystem.net>
2025-07-24 14:25:10 +0700

    26. error recovering

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,14 +1,27 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
+import { startTransition } from 'react';
 
-const Error = ({ error }: { error: Error }) => {
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
   const params = useParams();
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <h1>
       Error in reviewId {params.reviewId}
       <br />
       {error.message}
+      <div>
+        <button onClick={reload}>Try again</button>
+      </div>
     </h1>
   );
 };
