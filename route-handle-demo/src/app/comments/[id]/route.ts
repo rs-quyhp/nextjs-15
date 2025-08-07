bdd383c23365a9db8a1b7207ed7f574fe2a46207
quyhp <quyhp@runsystem.net>
2025-08-07 16:04:29 +0700

    39. handling PATCH request

--- route-handle-demo/src/app/comments/[id]/route.ts
+++ route-handle-demo/src/app/comments/[id]/route.ts
@@ -9,3 +9,15 @@ export const GET = async (
 
   return Response.json(comment);
 };
+
+export const PATCH = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await params;
+  const data = await request.json();
+  const index = comments.findIndex((comment) => comment.id.toString() === id);
+  comments[index].text = data.text;
+
+  return Response.json(comments[index]);
+};
