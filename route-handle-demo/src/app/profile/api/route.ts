24c003d60367ccd21b80217e9ac3ad9b9e3793e0
quyhp <quyhp@runsystem.net>
2025-08-08 11:49:46 +0700

    43. cookies in route handlers

--- route-handle-demo/src/app/profile/api/route.ts
+++ route-handle-demo/src/app/profile/api/route.ts
@@ -1,8 +1,15 @@
-import { headers } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 
 export const GET = async () => {
   const headerList = await headers();
+  const cookieList = await cookies();
+
+  cookieList.set('page', '1');
+  cookieList.set('limit', '100');
+  cookieList.delete('theme');
+
   console.log('Authorization: ', headerList.get('Authorization'));
+  console.log('Theme: ', cookieList.get('theme'));
   return new Response('<h1>Profile API data</h1>', {
     headers: {
       'Content-Type': 'text/html',
