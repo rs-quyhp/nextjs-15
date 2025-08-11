9d04b18a2a039badfa25ccbfd6cb2c5576bbaabe
quyhp <quyhp@runsystem.net>
2025-08-11 17:04:42 +0700

    62. context provider

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { ImageSlider } from '../components/imageSlider';
+import { useTheme } from '../components/themeProvider';
 
 const ClientRoutePage = () => {
-  return <ImageSlider />;
+  const theme = useTheme();
+  return <h1 style={{ color: theme.colors.secondary }}>Client route page</h1>;
 };
 
 export default ClientRoutePage;
