f1daf585154a8a1cf963b625330c52146bc19238
quyhp <quyhp@runsystem.net>
2025-08-11 17:18:11 +0700

    63. client-only code

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
-import { ImageSlider } from '../components/imageSlider';
 import { useTheme } from '../components/themeProvider';
+import { clientSideFunction } from '../utils/client-utils';
 
 const ClientRoutePage = () => {
   const theme = useTheme();
-  return <h1 style={{ color: theme.colors.secondary }}>Client route page</h1>;
+  const result = clientSideFunction();
+  return (
+    <h1 style={{ color: theme.colors.secondary }}>
+      Client route page {result}
+    </h1>
+  );
 };
 
 export default ClientRoutePage;
--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,6 +1,14 @@
 import { ImageSlider } from '../components/imageSlider';
+import { clientSideFunction } from '../utils/client-utils';
+
 const ServerRoutePage = () => {
-  return <ImageSlider />;
+  const result = clientSideFunction();
+  return (
+    <>
+      <ImageSlider />
+      <h1>Server page {result}</h1>
+    </>
+  );
 };
 
 export default ServerRoutePage;
