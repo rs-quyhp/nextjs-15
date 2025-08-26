b599be116919f0421cc5259b7eb0d78ad9a3e6eb
quyhp <quyhp@runsystem.net>
2025-08-26 11:08:01 +0700

    87. SignedIn & SignedOut

--- authentication-demo/src/components/navigations.tsx
+++ authentication-demo/src/components/navigations.tsx
@@ -1,4 +1,9 @@
-import { SignInButton, SignOutButton, UserButton } from '@clerk/nextjs';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from '@clerk/nextjs';
 import Link from 'next/link';
 
 export const Navigation = () => {
@@ -12,10 +17,14 @@ export const Navigation = () => {
             </h1>
           </div>
           <div className="flex items-center gap-4">
-            <SignInButton mode="modal" />
+            <SignedOut>
+              <SignInButton mode="modal" />
+            </SignedOut>
             {/* <UserButton /> */}
-            <Link href={'/user-profile'}>Profile</Link>
-            <SignOutButton />
+            <SignedIn>
+              <Link href={'/user-profile'}>Profile</Link>
+              <SignOutButton />
+            </SignedIn>
           </div>
         </div>
       </div>
