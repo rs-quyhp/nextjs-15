6a7beb307944c8f1d717083f75ce33fd1e75e372
quyhp <quyhp@runsystem.net>
2025-08-28 08:55:06 +0700

    88. protecting route

--- authentication-demo/src/middleware.ts
+++ authentication-demo/src/middleware.ts
@@ -1,6 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// const isProtectedRoute = createRouteMatcher(['/user-profile']);
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-out(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // if (!isPublicRoute(req)) await auth.protect();
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && !isPublicRoute(req)) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
